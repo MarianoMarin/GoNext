ee7798612c0604e1813bd4ff1b24609d58a9cd78
Mariano Marin <marianodmarin@gmail.com>
2021-09-05 12:40:24 -0500

    main: Added MaterialUI, established Layout, search bar, data.

--- go-next/src/App.js
+++ go-next/src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoNextLayout } from './components/GoNextLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className='welcome-text'>
-          <p>Welcome to Go Next!</p>
-          <p>We're still booting up, but our aim is to be</p>
-          <p>Your go-to place to find your next favorite game!</p>
-        </div>
-      </header>
+      <GoNextLayout />
     </div>
   );
 }
