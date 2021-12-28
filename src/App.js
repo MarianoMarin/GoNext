f76983e52e01b95d3a7ba95cc9e091215b0ef1c3
Mariano Marin <marianodmarin@gmail.com>
2021-12-28 14:04:57 -0600

    Correct file structure.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { GoNextLayout } from './components/GoNextLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <GoNextLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GoNextLayout.js
@@ -0,0 +1,27 @@
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { TextField } from '@material-ui/core';
+import games from '../data/games.json';
+
+export function GoNextLayout() {
+    const searchBar =
+    (<Autocomplete
+        id='game-search-bar'
+        options={games}
+        getOptionLabel={(game) => game.title}
+        renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+    />);
+
+
+    // return the constructed layout for the App to show the user
+    return (<>
+        {searchBar}
+        <p>
+            This is the <code>src/components/GoNextLayout.js</code> layout component.
+        </p>
+        <div className='welcome-text'>
+            <p>Welcome to Go Next!</p>
+            <p>We're still booting up, but our aim is to be</p>
+            <p>Your go-to place to find your next favorite game!</p>
+        </div>
+        </>);
+}
